cd5dbcc10d6435d761d30476aae1516f3f0c41eb
timothy <lai.timothy77@gmail.com>
2026-09-14 16:10:45 -0400

    feat: add syncedFromSteam field to Wishlist model
    Schema change plus migration (SQLite RedefineTables). Prisma client regenerated; also normalizes field alignment from prisma format.

---
+++ backend/prisma/migrations/20260914201016_added_synced_from_steam_attribute_to_wishlist/migration.sql
@@ -0,0 +1,19 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Wishlist" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "isDefault" BOOLEAN NOT NULL DEFAULT false,
+    "syncedFromSteam" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Wishlist_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Wishlist" ("createdAt", "description", "id", "isDefault", "name", "updatedAt", "userId") SELECT "createdAt", "description", "id", "isDefault", "name", "updatedAt", "userId" FROM "Wishlist";
+DROP TABLE "Wishlist";
+ALTER TABLE "new_Wishlist" RENAME TO "Wishlist";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
